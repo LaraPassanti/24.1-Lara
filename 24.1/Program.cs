0608bc31a8261d9bc2e14c561c658994a6f511a7
lara.passanti@gmail.com <lara.passanti@gmail.com>
2023-11-29 15:59:21 +0100

    Aggiungere i file di progetto.

---
+++ 24.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Quadrato a = new Quadrato();
+            Console.WriteLine("inserire lato");
+            a.Lato = float.Parse(Console.ReadLine());
+            float superficie = a.Lato * a.Lato;
+            Console.WriteLine("la superficie è di {0}", superficie);
+            Console.ReadLine();
+
+
+        }
+    }
+}
